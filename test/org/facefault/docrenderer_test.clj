da22db84a7502f50292c789b4f3f27ff3c388591
Brad Ackerman <brad@facefault.org>
2019-05-12 17:54:15 -0400

    Remove not-there-yet placeholder files.

--- test/org/facefault/docrenderer_test.clj
+++
@@ -1,7 +0,0 @@
-(ns org.facefault.docrenderer-test
-  (:require [clojure.test :refer :all]
-            [org.facefault.docrenderer :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
