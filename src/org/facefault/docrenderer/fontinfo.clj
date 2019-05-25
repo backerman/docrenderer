25f4d95c5fb1afce54dad98bfe74453c3a4a040e
Brad Ackerman <brad@facefault.org>
2019-05-25 15:37:04 -0400

    Prep to use this stuff.

--- src/org/facefault/docrenderer/fontinfo.clj
+++ src/org/facefault/docrenderer/fontinfo.clj
@@ -15,10 +15,8 @@
 ;; http://apache-fop.1065347.n5.nabble.com/Extract-font-width-using-FOP-td37360.html
 (defn font-info
   "Create a FontInfo object with our FOP configuration and register all fonts."
-  []
-  (let [factory render/my-factory
-        user-agent (.newFOUserAgent factory)
-        document-handler (-> factory
+  [factory user-agent]
+  (let [document-handler (-> factory
                              .getRendererFactory
                              (.createDocumentHandler user-agent
                                                      MimeConstants/MIME_PDF))
@@ -43,7 +41,9 @@
 (defn- display-fonts
   ;; Handy debugging routine.
   []
-  (let [info (font-info)]
+  (let [factory render/my-factory
+        user-agent (.newFOUserAgent factory)
+        info (font-info factory user-agent)]
     (run! (fn [tf-entry]
             (let [typeface (.getValue tf-entry)]
               (printf "Font: %s\nFamilies: %s\n\n"
