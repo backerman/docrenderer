44b95e1dce4530850e1c9b153989af7a170c5092
Brad Ackerman <brad@facefault.org>
2019-05-26 19:32:12 -0400

    Add support for producing area-tree XML.

--- src/org/facefault/docrenderer/render.clj
+++ src/org/facefault/docrenderer/render.clj
@@ -2,13 +2,16 @@
   (:import (org.apache.fop.apps Fop
                                 FopFactory
                                 MimeConstants)
-           (java.io OutputStreamWriter
+           org.apache.fop.render.xml.XMLRenderer
+           (java.io ByteArrayOutputStream
+                    OutputStreamWriter
                     FileOutputStream
                     File)
-           javax.xml.transform.stream.StreamSource)
+           javax.xml.transform.sax.SAXTransformerFactory
+           javax.xml.transform.stream.StreamResult)
   (:require [clojure.java.io :as io]
             [clojure.tools.logging :as log]
-            [org.facefault.docrenderer.transform :as xform]))
+            (org.facefault.docrenderer [transform :as xform])))
 
 (defn- copy-resource-to-file
   "Copies a classpath resource to a file, returning a java.io.File.
@@ -28,10 +31,32 @@
 
 (def my-factory (new-factory))
 
+(def sax-transformer-factory (SAXTransformerFactory/newInstance))
+
 (defn- new-fop
-  "Create a FOP from the factory and set to PDF output."
-  [factory output-stream]
-  (.newFop factory MimeConstants/MIME_PDF output-stream))
+  "Create a FOP from the factory. If a MIME type is not provided,
+  default to PDF."
+  ([factory output-stream]
+   (new-fop factory output-stream MimeConstants/MIME_PDF))
+  ([factory output-stream mime-type]
+   (.newFop factory mime-type output-stream)))
+
+(defn- xml-rendering-fop
+  ([factory output-stream]
+   (xml-rendering-fop factory output-stream MimeConstants/MIME_PDF))
+  ([factory output-stream mime-type]
+   (let [user-agent (.newFOUserAgent factory)
+         renderer (XMLRenderer. user-agent)
+         handler (.newTransformerHandler sax-transformer-factory)
+         result (StreamResult. output-stream)]
+     (.mimicRenderer renderer
+                     (-> user-agent
+                         .getRendererFactory
+                         (.createRenderer user-agent mime-type)))
+     (.setResult handler result)
+     (.setRendererOverride user-agent renderer)
+     (.setContentHandler renderer handler)
+     (.newFop factory MimeConstants/MIME_FOP_AREA_TREE user-agent))))
 
 (defn transform-and-render
   "Transform the input XML (in-stream) with the provided (compiled)
